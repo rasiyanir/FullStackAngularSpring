54679919d4c023d6f3ff3805cdb6927fad140e38
riyaz rasiyani <riyazr8016@gmail.com>
2021-06-15 15:32:36 -0500

    add get all employees in app.component.ts and other methods using httpclient in employeeservices.

--- employeemanagerapp/src/app/app.module.ts
+++ employeemanagerapp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
